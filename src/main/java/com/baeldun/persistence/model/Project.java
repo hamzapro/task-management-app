180840b1591bf47667663ea706dd80b496fbecc4
hamza <najihamzapro@gmail.com>
2021-10-19 21:42:23 +0200

    initial commit

---
+++ src/main/java/com/baeldun/persistence/model/Project.java
@@ -0,0 +1,12 @@
+package com.baeldun.persistence.model;
+
+
+import java.time.LocalDate;
+
+
+public class Project {
+    private Long id;
+    private String name;
+    private LocalDate dateCreated;
+
+}
---
+++ src/main/java/com/baeldun/taskmanagementapp/LsAppApplication.java
@@ -0,0 +1,13 @@
+package com.baeldun.taskmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsAppApplication.class, args);
+	}
+
+}
